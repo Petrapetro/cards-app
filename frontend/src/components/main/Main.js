70532e75a2b1bdfe819a43f1003b24eeac5eba9f
Petra Petro <petropetra93@gmail.com>
2021-03-07 23:13:56 +0100

    fix style: main to be responsive

--- frontend/src/components/main/Main.js
+++ frontend/src/components/main/Main.js
@@ -2,35 +2,14 @@ import React from 'react'
 import SignUp from '../../components/signup/SignUp'
 import Auth from '../../components/auth/Auth'
 import Cards from '../../components/card/Card'
+import './styles.css'
 
 const Main = () => {
   return (
-    <div style={{
-      textAlign: 'center',
-      width: '100%',
-      height: '100%',
-    }}>
-      <div style={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginTop: '15em',
-        
-      }}>
-        <div style={{
-          display: "block",
-          width: "25%",
-        }}>
-          <Cards
-            children={<Auth />} name="Login" /></div>
-        <div style={{
-          display: "block",
-          width: "25%"
-        }}>
-          <Cards
-            children={<SignUp />} name="Sign Up" /></div>
+      <div className="main-wrapper">
+        <div><Cards children={<Auth />} name="Login" /></div>
+        <div id="secondCard"><Cards children={<SignUp />} name="Sign Up" /></div>
       </div>
-    </div>
   )
 }
 
