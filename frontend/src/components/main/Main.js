606d487e37d2869e394a7b318e88830a81a8d766
Petra Petro <petropetra93@gmail.com>
2021-03-07 09:59:28 +0100

    frontend: fix Card component

--- frontend/src/components/main/Main.js
+++ frontend/src/components/main/Main.js
@@ -1,15 +1,34 @@
 import React from 'react'
 import SignUp from '../../components/signup/SignUp'
 import Auth from '../../components/auth/Auth'
+import Cards from '../../components/card/Card'
 
 const Main = () => {
-return (
-  <div>
-    <h1>Cards!</h1>
-    <Auth />
-    <SignUp />
-  </div>
-)
+  return (
+    <div style={{
+      textAlign: 'center'    }}>
+      <h1>Cards!</h1>
+      <div style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: '10em'
+      }}>
+        <div style={{
+          display: "inline-block",
+          width: "25%"
+        }}>
+          <Cards
+            children={<Auth />} name="Login" /></div>
+        <div style={{
+          display: "inline-block",
+          width: "25%"
+        }}>
+          <Cards
+            children={<SignUp />} name="Sign Up" /></div>
+      </div>
+    </div>
+  )
 }
 
 export default Main
