a7f8b8ecbe1820a3a302cdbc0f3133d61f0f91f0
Petra Petro <petropetra93@gmail.com>
2021-03-14 19:54:16 +0100

    feat: set form - add multiple cards to a set

--- frontend/src/components/addNewSet/SetForm.js
+++ frontend/src/components/addNewSet/SetForm.js
@@ -8,29 +8,39 @@ import React, { useState } from 'react'
 
 const SetForm = ({ userId }) => {
   const [cards, setCards] = useState([])
-  const [setname, setSetname] = useState(null)
-  const [text, setText] = useState(null)
-  const [flippedText, setFlippedText] = useState(null)
+  const [setname, setSetname] = useState(undefined)
+  const [text, setText] = useState(undefined)
+  const [flippedText, setFlippedText] = useState(undefined)
   const [words, setWords] = useState([[]])
 
-  const handleChangeSetname = (e) => {
+  const handleChange = (e) => {
     e.persist()
-    setSetname({[e.target.name]: e.target.value})
-  }
-  const handleChangeText = (e) => {
-    e.persist()
-    setText({[e.target.name]: e.target.value})
-  }
-
-  const handleChangeFlippedText = (e) => {
-    e.persist()
-    setFlippedText({[e.target.name]: e.target.value})
+    if (e.target.name === "flippedText") {
+      setFlippedText(e.target.value)
+    }
+    if (e.target.name === "text") {
+      setText(e.target.value)
+    }
+    if (e.target.name === "setname") {
+      setSetname(e.target.value)
+    }
   }
 
   const addWord = () => {
-    if(text !== null && flippedText !== null) {
-      setWords([... [text, flippedText]])
+    if (text !== undefined && flippedText !== undefined &&
+      text !== '' && flippedText !== '') {
+      if(words[0] === []) {
+        console.log({words})
+        setWords({text, flippedText})
+        setWords(words.shift())
+      } else {
+        setWords([...words, {text, flippedText}])
+      }
+      console.log({ words })
+      setText('')
+      setFlippedText('')
     }
+
   }
 
   const handleSubmit = () => {
@@ -40,48 +50,49 @@ const SetForm = ({ userId }) => {
     <div>
       <h1>Create new Study Set</h1>
       <form onSubmit={handleSubmit}>
-          <TextField
-            type="setname"
-            placeholder="setname"
-            name="setname"
-            id="setname"
-            color="primary"
-            value={setname}
-            onChange={handleChangeSetname}
-          />
-          {words.map(() => (
-          <FormControl>
-          <TextField
-            type="text"
-            placeholder="text"
-            name="text"
-            id="text"
-            color="primary"
-            value={text}
-            onChange={handleChangeText}
-          />
-          <TextField
-            type="flippedText"
-            placeholder="flippedText"
-            name="flippedText"
-            id="flippedText"
-            color="primary"
-            value={flippedText}
-            onChange={handleChangeFlippedText}
-          />
-          </FormControl>
-          ))}
+        <TextField
+          type="setname"
+          placeholder="setname"
+          name="setname"
+          id="setname"
+          color="primary"
+          value={setname}
+          onChange={(e) => handleChange(e)}
+        />
+        {words.map(({text, flippedText}, index) => (
+          index > 0 && index !== words.length-1 &&
+          <><p>{`${index}`} Text: {`${text}`} Flipped Text: {`${flippedText}`}</p></>))}
+              <FormControl>
+                <TextField
+                  type="text"
+                  placeholder="text"
+                  name="text"
+                  id="text"
+                  color="primary"
+                  value={text}
+                  onChange={(e) => handleChange(e)}
+                />
+                <TextField
+                  type="flippedText"
+                  placeholder="flippedText"
+                  name="flippedText"
+                  id="flippedText"
+                  color="primary"
+                  value={flippedText}
+                  onChange={(e) => handleChange(e)}
+                />
+              </FormControl>
           <Button onClick={addWord}>Add another word</Button>
-        <div>
-          <Button
-            variant="contained"
-            color="secondary"
-            type="submit"
-          >
-            Create Set!
+              <div>
+                <Button
+                  variant="contained"
+                  color="secondary"
+                  type="submit"
+                >
+                  Create Set!
             </Button>
-        </div>
-      </form>
+              </div>
+            </form>
     </div>
   )
 }
